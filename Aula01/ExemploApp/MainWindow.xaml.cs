55cb7720eab6cf8d21e01b0eda9c0c442f44ab0d
JordsonZ <jordsongalvao@gmail.com>
2022-10-14 17:37:48 -0300

    Atualização do Exemplo01

--- Aula01/ExemploApp/MainWindow.xaml.cs
+++ Aula01/ExemploApp/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace ExemploApp
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hello World");
+        }
     }
 }
