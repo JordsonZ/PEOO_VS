a49aa1fb8d4c0eeabd144c28e6a57f78771df8ce
JordsonZ <jordsongalvao@gmail.com>
2022-10-21 16:50:47 -0300

    novo

---
+++ Aula02/AgendaApp/Contato.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgendaApp
+{
+    class Contato
+    {
+        private string nome, email, fone;
+        public Contato(string n, string e, string f)
+        {
+            nome = n;
+            email = e;
+            fone = f;
+        }
+        public override string ToString()
+        {
+            return $"{nome} - {email} - {fone}";
+        }
+    }
+}
---
+++ Aula02/AgendaApp/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AgendaApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //MessageBox.Show(txtNome.Text);
+            Contato c = new Contato(txtNome.Text, txtEmail.Text, txtFone.Text);
+            MessageBox.Show(c.ToString());
+            list.Items.Add(c);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (list.SelectedItem != null)
+            list.Items.Remove(list.SelectedItem);
+        }
+    }
+}
