2759a4b023b6c3e5cd67afa3689c50cb7d46cc00
JordsonZ <105514196+JordsonZ@users.noreply.github.com>
2022-10-24 15:50:58 -0300

    Add files via upload
    upload

---
+++ Aula04/Abastecimento/Abastecimento.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbastecimentoApp
+{
+    class Abastecimento
+    {
+        private double valorLt, valorPg, KmRodados;
+
+        public void SetValorLt(double valorLt)
+        {
+            if (valorLt > 0) this.valorLt = valorLt;
+        }
+        public void SetValorPg(double valorPg)
+        {
+            if (valorPg > 0) this.valorPg = valorPg;
+        }
+        public void SetKmRodados(double KmRodados)
+        {
+            if (KmRodados > 0) this.KmRodados = KmRodados;
+        }
+        public double MediaKmLitro()
+        {
+            double numLitros = valorPg / valorLt;
+            return KmRodados / numLitros;
+        }
+        public double MediaReaisKm()
+        {
+            return valorPg / KmRodados;
+        }
+    }
+    
+}
---
+++ Aula04/Abastecimento/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AbastecimentoApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Abastecimento obj = new Abastecimento();
+            obj.SetKmRodados(double.Parse(txt1.Text));
+            obj.SetValorPg(double.Parse(txt2.Text));
+            obj.SetValorLt(double.Parse(txt3.Text));
+            txt4.Text = obj.MediaKmLitro().ToString();
+            txt5.Text = obj.MediaReaisKm().ToString();
+        }
+    }
+}
