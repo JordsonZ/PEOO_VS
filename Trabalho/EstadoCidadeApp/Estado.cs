16d5787ac0176e46166bc284c8f28f068d471232
JudilenaGalvao <judilena.galvao.707@ufrn.edu.br>
2022-12-16 23:39:11 -0300

    trabalho

---
+++ Trabalho/EstadoCidadeApp/Estado.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EstadoCidadeApp
+{
+    class Estado
+    {
+        private string nome;
+        private double area;
+        private Cidade[] cidades = new Cidade[10];
+        private int k = 0;
+
+        public Estado(string n)
+        {
+            nome = n;
+        }
+        public void SetArea(double a)
+        {
+            area = a;
+        }
+        public double GetArea()
+        {
+            return area;
+        }
+        public void Inserir(Cidade c)
+        {
+            cidades[k] = c;
+            k++;
+        }
+        public Cidade[] Listar()
+        {
+            Cidade[] vetor = new Cidade[k];
+            Array.Copy(cidades, vetor, k);
+            return vetor;
+        }
+        public int Populacao()
+        {
+            int Total;
+            int populacao = 0;
+            for (int i = 1; i >= k; i++)
+            {
+                Total = populacao++;
+            }
+            return Populacao();
+        }         
+        public override string ToString()
+        {
+            return $"{Listar()} - {nome} - {Populacao()}";
+        }
+    }
+    class Cidade : object
+    {
+        private string nome;
+        private double area;
+        private int populacao;
+        public Cidade(string n)
+        {
+            nome = n;
+        }
+        public void SetArea(double a)
+        {
+            this.area = a;
+        }
+        public void SetPopulacao(int p)
+        {
+            this.populacao = p;
+        }
+        public string GetNome()
+        {
+            return nome;
+        }
+        public int GetPopulacao()
+        {
+            return this.populacao;
+        }
+        public override string ToString()
+        {
+            return $"{nome} - {area} - {populacao}";
+        }
+    }
+}
---
+++ Trabalho/EstadoCidadeApp/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EstadoCidadeApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private Estado n;
+        private Cidade c;
+        private void inserir_Click(object sender, RoutedEventArgs e)
+        {
+            n = new Estado(txt1.Text);
+            Cidade c = new Cidade(txt2.Text);
+            c.SetArea(double.Parse(txt3.Text));
+            c.SetPopulacao(int.Parse(txt4.Text));
+            List.ItemsSource = null;
+            List.ItemsSource = c.ToString();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"{n.Populacao()}");
+        }
+    }
+}
